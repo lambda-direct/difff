ffdb5a2b87fdb8a7055680b681072cafff8fce8d
Kirill Usichenko <kirillusichenko@gmail.com>
2024-02-08 12:01:41 +0100

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+    import GitHubIcon from "~/lib/icons/GitHubIcon.svelte";
+</script>
+
+<svelte:head>
+    <title>Difff | compare and format json online</title>
+    <meta
+        name="description"
+        content="Compare two JSON documents. Validate, format, and .  See the differences between the objects."
+    />
+</svelte:head>
+
+<header class="header">
+    <h1>Compare JSON online</h1>
+    <a
+        href="https://github.com/lambda-direct/difff"
+        target="_blank"
+        rel="noopener"
+        title="Diff GitHub repository"
+        class="github-link"
+    >
+        <GitHubIcon />
+    </a>
+</header>
+<main class="compare-jsons_main">
+    <h2>Check JSONs differences</h2>
+    <section class="compare-jsons_fields"></section>
+</main>
+
+<style>
+    .header {
+        position: relative;
+        text-align: center;
+    }
+    .github-link {
+        position: absolute;
+        top: 0;
+        right: 0;
+    }
+    .compare-jsons_main {
+        text-align: center;
+    }
+    .compare-jsons_fields {
+    }
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { sveltekit } from "@sveltejs/kit/vite";
+import path from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    plugins: [sveltekit()],
+    resolve: {
+        alias: {
+            "~": path.resolve("./src")
+        }
+    }
+});
