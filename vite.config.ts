49db14f5c89ff07e1d80a33a55d038db812178bb
David Hordiienko <neonz96@mail.ru>
2024-03-19 12:08:32 +0200

    fix: add vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,25 @@ import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
+const VitePluginRemoveHashFontFiles = () => {
+    return {
+        name: "Remove hash from font files",
+        apply: "build",
+        enforce: "post",
+        config(config: any) {
+            const defaultAssetFileNames = config.build.rollupOptions.output.assetFileNames;
+            config.build.rollupOptions.output.assetFileNames = ({ type, name }) => {
+                if (type === "asset" && /\.(woff2?|ttf|otf)$/.test(name)) return `[name].[ext]`;
+                return defaultAssetFileNames;
+            };
+        }
+    };
+};
 export default defineConfig({
     optimizeDeps: {
         exclude: ["@biomejs/wasm-bundler", "@codemirror/*"]
     },
-    plugins: [sveltekit(), wasm(), topLevelAwait()],
+    plugins: [sveltekit(), wasm(), topLevelAwait(), VitePluginRemoveHashFontFiles()],
     resolve: {
         alias: {
             "~": path.resolve("./src"),
