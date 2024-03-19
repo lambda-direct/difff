4b606ecc88d8648cfd324f0021c391dd60c4fbce
David Hordiienko <neonz96@mail.ru>
2024-03-19 11:57:04 +0200

    fix: font preload

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,19 +3,6 @@
     import Footer from "~/lib/shared/Footer.svelte";
 </script>
 
-{@html `<link rel="preload" href="/fonts/JetBrainsMono-Regular.ttf" as="font" type="font/ttf" crossorigin />`}
-{@html `<link rel="preload" href="/fonts/JetBrainsMono-SemiBold.ttf" as="font" type="font/ttf" crossorigin />`}
-{@html `<link rel="preload" href="/fonts/NotoSans-Regular.ttf" as="font" type="font/ttf" crossorigin />`}
-{@html `<link rel="preload" href="/fonts/NotoSans-Bold.ttf" as="font" type="font/ttf" crossorigin />`}
-{@html `<link rel="preload" href="/fonts/SourceSerif4-BoldItalic.ttf" as="font" type="font/ttf" crossorigin />`}
-{@html `<link rel="preload" href="/fonts/Georgia.ttf" as="font" type="font/ttf" crossorigin />`}
-{@html `<link rel="preload" href="/fonts/Cambria.ttf" as="font" type="font/ttf" crossorigin />`}
-{@html `<link rel="preload" href="/fonts/TimesRegular.ttf" as="font" type="font/ttf" crossorigin />`}
-{@html `<link rel="preload" href="/fonts/timesnewroman.ttf" as="font" type="font/ttf" crossorigin />`}
-{@html `<link rel="preload" href="/fonts/ArialMT.ttf" as="font" type="font/ttf" crossorigin />`}
-{@html `<link rel="preload" href="/fonts/Helvetica.ttf" as="font" type="font/ttf" crossorigin />`}
-{@html `<link rel="preload" href="/fonts/HelveticaNeueBlackCondensed.ttf" as="font" type="font/ttf" crossorigin />`}
-
 <div class="page">
     <slot />
     <Footer />
