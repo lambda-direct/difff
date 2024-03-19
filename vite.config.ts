1fa6f3a4a52a820e9341c1eb75e527a66948a401
David Hordiienko <neonz96@mail.ru>
2024-03-19 12:11:02 +0200

    fix: remove font pluging

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,11 @@ import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
-const VitePluginRemoveHashFontFiles = () => {
-    return {
-        name: "Remove hash from font files",
-        apply: "build",
-        enforce: "post",
-        config(config: any) {
-            const defaultAssetFileNames = config.build.rollupOptions.output.assetFileNames;
-            config.build.rollupOptions.output.assetFileNames = ({ type, name }) => {
-                if (type === "asset" && /\.(woff2?|ttf|otf)$/.test(name)) return `[name].[ext]`;
-                return defaultAssetFileNames;
-            };
-        }
-    };
-};
 export default defineConfig({
     optimizeDeps: {
         exclude: ["@biomejs/wasm-bundler", "@codemirror/*"]
     },
-    plugins: [sveltekit(), wasm(), topLevelAwait(), VitePluginRemoveHashFontFiles()],
+    plugins: [sveltekit(), wasm(), topLevelAwait()],
     resolve: {
         alias: {
             "~": path.resolve("./src"),
